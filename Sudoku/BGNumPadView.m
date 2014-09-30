cf8f81fa4b871728f562a066fb87b1dfa930fc49
Sarah Gilkinson <sgilkinson@hmc.edu>
2014-09-29 20:32:42 -0700

    Added erase button.

--- Sudoku/BGNumPadView.m
+++ Sudoku/BGNumPadView.m
@@ -24,26 +24,30 @@
         // Separation between cells in different blocks
         CGFloat size = frame.size.width;
         const CGFloat separation = 0.01*size;
-        self.backgroundColor = [UIColor blackColor];
+        self.backgroundColor = [UIColor clearColor];
         
         // Leaving 0.01 each for four lines separating blocks
-        CGFloat buttonSize = (size-4*separation)/9.0;
+        CGFloat buttonSize = (size - 4*separation)/10.0;
         
         // Array to hold 9 buttons
         _buttonArray = [[NSMutableArray alloc] init];
         
         // create button
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             // Create the button
             UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(i*buttonSize + 2*separation,separation, buttonSize, buttonSize)];
             
             button.backgroundColor = [UIColor whiteColor];
             [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-            [button setTitle:[NSString stringWithFormat:@"%i",i+1] forState:UIControlStateNormal];
+            if (i == 0) {
+                [button setTitle:@"" forState:UIControlStateNormal];
+            } else {
+                [button setTitle:[NSString stringWithFormat:@"%i",i] forState:UIControlStateNormal];
+            }
             button.titleLabel.font = [UIFont boldSystemFontOfSize:[UIFont buttonFontSize]];
             
             // Tag of 21 represents second row, first column
-            [button setTag:i+1];
+            [button setTag:i];
             [button addTarget:self action:@selector(buttonPressed:)forControlEvents:UIControlEventTouchUpInside];
             
             [button.layer setBorderWidth:2.0f];
@@ -68,8 +72,8 @@
 
 - (void)setSelectedNumber:(int)selection
 {
-    UIButton *oldSelection = (UIButton *) _buttonArray[_numberSelected -1];
-    UIButton *newSelection = (UIButton *) _buttonArray[selection-1];
+    UIButton *oldSelection = (UIButton *) _buttonArray[_numberSelected];
+    UIButton *newSelection = (UIButton *) _buttonArray[selection];
     oldSelection.backgroundColor = [UIColor whiteColor];
     newSelection.backgroundColor = [UIColor yellowColor];
     _numberSelected = selection;
